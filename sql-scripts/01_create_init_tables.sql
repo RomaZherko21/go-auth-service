4603d11eb3ec3d8680e0602bb07704f6cb118e61
RomaZherko210 <r.zherko@invatechs.com>
2023-11-24 11:54:05 +0300

    remove sql script

--- sql-scripts/01_create_init_tables.sql
+++
@@ -1,8 +0,0 @@
--- 01_create_tables.sql
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
-
--- Add more SQL statements as needed
