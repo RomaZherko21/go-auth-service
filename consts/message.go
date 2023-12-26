4f2f247d60fc8b1c93e0541f84f99d764f99336a
RomaZherko210 <r.zherko@invatechs.com>
2023-12-27 02:14:58 +0300

    change user messages

---
+++ consts/message.go
@@ -0,0 +1,4 @@
+package consts
+
+const SOMETHING_WENT_WRONG = "something went wrong, please contact support"
+const INVALID_BODY = "invalid body"
--- entities/auth/handler.go
+++ entities/auth/handler.go
@@ -14,70 +14,70 @@ import (
 )
 
 func SignIn(c *gin.Context) {
+	const USER_ERROR_MESSAGE = "wrong email or password"
 	var body User
 
 	if err := c.ShouldBindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": USER_ERROR_MESSAGE})
+		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("%v: %v", consts.INVALID_BODY, err.Error()))
 		return
 	}
 
 	userMeta, err := UserServiceInstance.GetUserPassword(c, &body)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": USER_ERROR_MESSAGE})
+		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("cant't get user from db: %v", err.Error()))
 		return
 	}
 
 	if isPasswordCorrect := helpers.CheckPassword(body.Password, userMeta.Password); !isPasswordCorrect {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "wrong email or password"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": USER_ERROR_MESSAGE})
 		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("wrong email or password on uid: %v", userMeta.ID))
 		return
 	}
 
 	accessDetails, err := helpers.CreateAccessToken(userMeta.ID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "cant create access token"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("can't create access token: %v", err.Error()))
 		return
 	}
 
 	refreshDetails, err := helpers.CreateRefreshToken(c.Request.Header, userMeta.ID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "cant create refresh token"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("can't create refresh token: %v", err.Error()))
 		return
 	}
 
 	err = helpers.SetAccessTokenCookie(c, accessDetails.AccessToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set access token to cookie"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set access token to cookie: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("can't set access token to cookie: %v", err.Error()))
 		return
 	}
 
 	err = helpers.SetRefreshTokenCookie(c, refreshDetails.RefreshToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set refresh token to cookie"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set refresh token to cookie: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("can't set refresh token to cookie: %v", err.Error()))
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "user authenticated"})
-	log.HttpLog(c, log.Warn, http.StatusOK, "user authenticated")
+	log.HttpLog(c, log.Warn, http.StatusOK, fmt.Sprintf("user authenticated: %v", userMeta.ID))
 }
 
 func SignUp(c *gin.Context) {
 	var user User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": consts.INVALID_BODY})
+		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("%v: %v", consts.INVALID_BODY, err.Error()))
 		return
 	}
 
 	validationResult := helpers.Validate(&user)
-
 	if !validationResult.OK {
 		c.JSON(http.StatusBadRequest, gin.H{"error": validationResult.Errors})
 		log.HttpLog(c, log.Warn, http.StatusBadRequest, "validation error")
@@ -85,74 +85,67 @@ func SignUp(c *gin.Context) {
 	}
 
 	password, err := helpers.HashPassword(user.Password)
-
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't hash password: %v", err.Error()))
 		return
 	}
 
 	user.Password = password
 
 	if err := UserServiceInstance.CreateUser(c, &user); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't create user: %v", err.Error()))
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
-	log.HttpLog(c, log.Info, http.StatusOK, "User created successfully")
+	c.JSON(http.StatusOK, gin.H{"message": "user was created successfully"})
+	log.HttpLog(c, log.Info, http.StatusOK, fmt.Sprintf("user was created successfully: %v", user.Email))
 }
 
 func SignOut(c *gin.Context) {
 	refreshToken, err := c.Cookie(consts.REFRESH_TOKEN)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "can't extract refresh token"})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "no refresh token"})
+		log.HttpLog(c, log.Error, http.StatusBadRequest, fmt.Sprintf("can't extract refresh token: %v", err.Error()))
 		return
 	}
 
-	// add refresh token to black list
 	redisDb := c.MustGet("redis_db").(*redis.Client)
-
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
-		return
-	}
 
 	err = helpers.SetRefreshTokenToRedis(redisDb, refreshToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set tokens to redis"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set tokens to redis: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("сan't set tokens to redis: %v", err.Error()))
 		return
 	}
 
 	c.SetCookie(consts.ACCESS_TOKEN, "", -1, "/", "", false, true)
 	c.SetCookie(consts.REFRESH_TOKEN, "", -1, "/", "", false, true)
 
-	c.JSON(http.StatusUnauthorized, gin.H{"message": "User sign out"})
-	log.HttpLog(c, log.Info, http.StatusUnauthorized, "User sign out")
+	c.JSON(http.StatusUnauthorized, gin.H{"message": "user sign out"})
+	log.HttpLog(c, log.Info, http.StatusUnauthorized, "user sign out")
 }
 
 func Refresh(c *gin.Context) {
 	refreshToken, err := c.Cookie(consts.REFRESH_TOKEN)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "cant extract refresh token"})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "no refresh token"})
+		log.HttpLog(c, log.Error, http.StatusBadRequest, fmt.Sprintf("can't extract refresh token: %v", err.Error()))
 		return
 	}
 
 	claims, err := helpers.ParseToken(refreshToken, helpers.GetEnv("REFRESH_TOKEN_SECRET"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "cant parse refresh token"})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "refresh token is invalid"})
+		log.HttpLog(c, log.Error, http.StatusBadRequest, fmt.Sprintf("refresh token is invalid: %v", err.Error()))
 		return
 	}
 
 	userId, ok := claims["user_id"].(string)
 	if !ok {
-		c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, "сan't extract user_id claim")
 		return
 	}
 
@@ -160,36 +153,36 @@ func Refresh(c *gin.Context) {
 
 	err = helpers.SetRefreshTokenToRedis(redisDb, refreshToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set tokens to redis"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set tokens to redis: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't set tokens to redis: %v", err.Error()))
 		return
 	}
 
 	accessDetails, err := helpers.CreateAccessToken(userId)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "cant create access token"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't create access token: %v", err.Error()))
 		return
 	}
 
 	refreshDetails, err := helpers.CreateRefreshToken(c.Request.Header, userId)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "cant create refresh token"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't create refresh token: %v", err.Error()))
 		return
 	}
 
 	err = helpers.SetAccessTokenCookie(c, accessDetails.AccessToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set tokens to cookie"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set access to cookie: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't set access to cookie: %v", err.Error()))
 		return
 	}
 
 	err = helpers.SetRefreshTokenCookie(c, refreshDetails.RefreshToken)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cant set tokens to cookie"})
-		log.HttpLog(c, log.Warn, http.StatusInternalServerError, fmt.Sprintf("Cant set refresh to cookie: %v", err.Error()))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, fmt.Sprintf("can't set refresh to cookie: %v", err.Error()))
 		return
 	}
 
@@ -202,8 +195,8 @@ func SignOutFromAllDevices(c *gin.Context) {
 
 	userId, ok := c.Get("user_id")
 	if !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "can't extract user id"})
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, "can't extract user id")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": consts.SOMETHING_WENT_WRONG})
+		log.HttpLog(c, log.Error, http.StatusInternalServerError, "can't extract user_id claim")
 		return
 	}
 
@@ -212,22 +205,22 @@ func SignOutFromAllDevices(c *gin.Context) {
 	for iter.Next(ctx) {
 		currentValue, err := redisDb.Get(ctx, iter.Val()).Result()
 		if err != nil {
-			log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("Cant remove refresh token of user: %v. Err: %v", userId, err.Error()))
+			log.HttpLog(c, log.Warn, http.StatusUnprocessableEntity, fmt.Sprintf("can't remove refresh token of user: %v. Err: %v", userId, err.Error()))
 		}
 
 		if currentValue == userId {
 			if err := redisDb.Del(context.Background(), iter.Val()).Err(); err != nil {
-				log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("Cant remove refresh token: %v, of user: %v. Err: %v", currentValue, userId, err.Error()))
+				log.HttpLog(c, log.Warn, http.StatusUnprocessableEntity, fmt.Sprintf("can't remove refresh token: %v, of user: %v. Err: %v", currentValue, userId, err.Error()))
 			}
 		}
 	}
 	if err := iter.Err(); err != nil {
-		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("Cant remove refresh token of user: %v. Err: %v", userId, err.Error()))
+		log.HttpLog(c, log.Warn, http.StatusBadRequest, fmt.Sprintf("can't remove refresh token of user: %v. Err: %v", userId, err.Error()))
 	}
 
 	c.SetCookie(consts.ACCESS_TOKEN, "", -1, "/", "", false, true)
 	c.SetCookie(consts.REFRESH_TOKEN, "", -1, "/", "", false, true)
 
-	c.JSON(http.StatusUnauthorized, gin.H{"message": "User sign out from all devices"})
-	log.HttpLog(c, log.Info, http.StatusUnauthorized, "User sign out from all devices")
+	c.JSON(http.StatusUnauthorized, gin.H{"message": "user sign out from all devices"})
+	log.HttpLog(c, log.Info, http.StatusUnauthorized, "user sign out from all devices")
 }
